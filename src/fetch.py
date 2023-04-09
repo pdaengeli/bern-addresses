76d05a0354b1930b105ac587616d2f5f30690351
Sascha Brawer <sascha@brawer.ch>
2023-04-09 10:58:58 +0200

    Skip ads pages; synthesize labels for first page in chapter

--- src/fetch.py
+++ src/fetch.py
@@ -3,26 +3,6 @@
 #
 # Fetch address books of Bern, 1861-1945, from www.e-rara.ch.
 
-# TODO: Check page labels for volume 1911-1912:
-# - Page(id=25875700, label='272')
-# - Page(id=25875701, label=None)    <-- is there really no page number?
-# - Page(id=25875702, label=None)    <-- is there really no page number?
-# - Page(id=25875703, label='273')
-#
-# TODO: Check page labels for volume 1914:
-# - Page(id=25877340, label='304')
-# - Page(id=25877341, label=None)    <-- is there really no page number?
-# - Page(id=25877342, label=None)    <-- is there really no page number?
-# - Page(id=25877343, label='305')
-#
-# TODO: Check page labels for volume 1917:
-# - Page(id=25878846, label='107')
-# - Page(id=25879533, label='110')    <-- should this be '108'?
-# - Page(id=25879534, label=None)     <-- should this be '109'?
-# - Page(id=25879535, label=None)     <-- should this be '110'?
-# - Page(id=25879536, label='111')
-
-
 from collections import namedtuple
 import csv
 import requests
@@ -38,10 +18,11 @@ def fetch_all():
     if not os.path.exists("cache"):
         os.mkdir("cache")
     for chapter in fetch_chapters():
+        first_page = chapter.pages[0]
         for page in chapter.pages:
-            # print(page)
-            fetch_page_xml(page.id)
-            fetch_page_plaintext(page.id)
+            if page == first_page or page.label != None:
+                fetch_page_xml(page.id)
+                fetch_page_plaintext(page.id)
         print(chapter.date, len(chapter.pages), chapter.pages[:3])
 
 
@@ -57,12 +38,13 @@ def fetch_chapters():
             date = row['Date']
             if len(date) == 4:
                 date = '%04d-12-15' % (int(date) - 1)
+            pages=fetch_chapter_pages(volume_id, chapter_id)
             chapter = Chapter(id=chapter_id,
                               title=row['ChapterTitle'],
                               date=date,
                               year=row['Year'],
                               volume=volume_id,
-                              pages=fetch_chapter_pages(volume_id, chapter_id))
+                              pages=pages)
             chapters.append(chapter)
     return chapters
 
@@ -85,7 +67,23 @@ def fetch_chapter_pages(volume_id, chapter_id):
             link_to = link.attrib['{http://www.w3.org/1999/xlink}to']
             page_id = int(link_to.removeprefix('phys'))
             page = Page(id=page_id, label=page_labels.get(page_id))
-            pages.append(page)
+
+            # The first page in a chapter typically has no label (page number):
+            # - https://www.e-rara.ch/bes_1/periodical/pageview/26021757
+            is_first_page = (len(pages) == 0)
+
+            # Other pages always have a label, unless they’re an advertisment:
+            # - https://www.e-rara.ch/bes_1/periodical/pageview/25875701
+            # - https://www.e-rara.ch/bes_1/periodical/pageview/25877342
+            is_advertisment_page = (page.label == None)
+
+            if is_first_page or not is_advertisment_page:
+                pages.append(page)
+
+    # If the first page in the chapter has no number, synthesize it: '[123]'.
+    if not pages[0].label:
+        pages[0] = Page(id=pages[0].id,
+                        label = f'[%d]' % (int(pages[1].label) - 1))
     return pages
 
 
